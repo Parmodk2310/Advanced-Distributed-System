5ccd76051903b1a20a978cd2e6f3802199c222ea
Parmodk2310 <121942961+Parmodk2310@users.noreply.github.com>
2026-09-15 09:01:54 +0530

    style(phase6): make release gate assertion black-stable

--- tests/unit/test_phase6_release_gate_contract.py
+++ tests/unit/test_phase6_release_gate_contract.py
@@ -7,9 +7,9 @@ def test_release_gate_waits_for_circuit_breaker_recovery_before_benchmarks() -> 
     chaos_end = script.index('done\n', script.index("for scenario in"))
     recovery_wait = script.index('sleep "$BREAKER_RECOVERY_WAIT_SECONDS"', chaos_end)
     task_benchmark = script.index("--workload task", recovery_wait)
-
-    assert (
+    expected_wait_setting = (
         'BREAKER_RECOVERY_WAIT_SECONDS="${PHASE6_BREAKER_RECOVERY_WAIT_SECONDS:-11}"'
-        in script
     )
+
+    assert expected_wait_setting in script
     assert chaos_end < recovery_wait < task_benchmark
